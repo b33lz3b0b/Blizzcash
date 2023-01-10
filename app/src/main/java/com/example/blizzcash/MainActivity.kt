220bbc092040835dfbd8f7d3cd7bb10837f2e815
amali <amaliaelenagrigore25@gmail.com>
2023-01-10 16:55:51 +0200

    hello

--- app/src/main/java/com/example/blizzcash/MainActivity.kt
+++ app/src/main/java/com/example/blizzcash/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.blizzcash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent{
+            MyFirstText()
+        }
     }
+}
+
+@Composable
+fun MyFirstText(){
+    Text(text = "hello world!")
 }
